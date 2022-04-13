35f84167872a3be31aa0013f4168c60ca660d0d9
velheor <vlad.atsman@gmail.com>
2022-04-14 01:56:20 +0300

    Added home page.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Car } from '../../models/car.model';
-import { CarHttpService } from '../../services/http/car-http.service';
-import { Sort } from '../../models/search.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   cars: Car[];
 
-  constructor(private carHttpService: CarHttpService) {
+  constructor() {
     this.cars = [];
   }
 
-  ngOnInit(): void {
-    this.carHttpService.getCarsPageable(0, 10, Sort.Latest).subscribe((cars: Car[]) => {
-      this.cars = cars;
-    });
-  }
+
 }
