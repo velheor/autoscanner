3d70c5455b3b57e59353dbe3065eeb7ef42f8ae8
velheor <vlad.atsman@gmail.com>
2022-06-06 01:46:47 +0300

    Refactoring.

--- src/app/components/search-car/search-car.component.ts
+++ src/app/components/search-car/search-car.component.ts
@@ -3,6 +3,7 @@ import { CarHttpService } from '../../services/http/car-http.service';
 import { Make } from '../../models/make.model';
 import { Model } from '../../models/model.model';
 import { Car } from '../../models/car.model';
+import { SearchCriteria } from '../../models/search.model';
 
 @Component({
   selector: 'app-search-car',
@@ -10,38 +11,49 @@ import { Car } from '../../models/car.model';
   styleUrls: ['./search-car.component.scss']
 })
 export class SearchCarComponent implements OnInit {
-
-  selectedMake: Make = {
-    name: '',
-    models: []
-  };
-  selectedModel: Model = {
-    name: ''
-  };
+  searchCriteria: SearchCriteria = new SearchCriteria;
   makes: Make[] = [];
   models: Model[] = [];
   cars: Car[] = [];
+  years: number[] = [];
+  engineSizes: number[] = [];
+  fuelTypes: FuelType[] = [FuelType.Gasoline, FuelType.Diesel, FuelType.Ethanol];
 
   constructor(private carHttpService: CarHttpService) {
   }
 
   ngOnInit(): void {
-    this.carHttpService.getMakes().subscribe(makes => {
-      this.makes = makes;
-    });
+    // this.carHttpService.getMakes().subscribe(makes => {
+    //   this.makes = makes;
+    // });
+    this.years = this.createYears(1910, 2022);
+    this.engineSizes = this.createEngineSizes(1.0, 9.0);
   }
 
   onSelectMake() {
-    console.log(this.selectedMake);
-    this.models = this.selectedMake.models;
+    this.models = this.searchCriteria.make.models;
   }
 
   filterCars() {
-    this.carHttpService.filterCars({
-      brand: this.selectedMake.name,
-      model: this.selectedModel.name,
-    }).subscribe(cars => {
-      this.cars = cars;
-    });
+    this.carHttpService.filterCars(this.searchCriteria)
+      .subscribe(cars => {
+        this.cars = cars;
+      });
+  }
+
+  createYears(start: number, end: number) {
+    let items: number[] = [];
+    for (let i = end; i >= start; i--) {
+      items.push(i);
+    }
+    return items;
+  }
+
+  createEngineSizes(start: number, end: number) {
+    let items: number[] = [];
+    for (let i = end; i >= start; i = i - 0.1) {
+      items.push(Number(i.toFixed(1)));
+    }
+    return items;
   }
 }
--- src/app/models/search.model.ts
+++ src/app/models/search.model.ts
@@ -1,8 +1,18 @@
+import { Make } from './make.model';
+import { Model } from './model.model';
+
 export class SearchCriteria {
-  brand?: string;
-  model?: string;
-  fromYear?: number;
-  toYear?: number;
+  make: Make = {
+    name: '',
+    models: []
+  };
+  model: Model = {
+    name: ''
+  };
+  fromYear: number = 0;
+  toYear: number = 0;
+  fromEngineSize: number = 0;
+  toEngineSize: number = 0;
   fromPrice?: number;
   toPrice?: number;
 }
